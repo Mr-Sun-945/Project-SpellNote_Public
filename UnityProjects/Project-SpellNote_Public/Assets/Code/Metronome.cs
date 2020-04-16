fa4d030a6188d2492ef382c53b8055ff1dbdce8f
CDobias <cdobias14@gmail.com>
2020-04-15 19:08:27 -0700

    Adding Asset Folders and Assets
    Adding:
    - Folders (/code, /code/Behavior Scripts, /Audio)
    - Some test code for playing audio
    - tick.wav, basically a metronome click tone

---
+++ UnityProjects/Project-SpellNote_Public/Assets/Code/Metronome.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Metronome : MonoBehaviour
+{
+    public float bpm = 60.0f;
+    public int numBeatsPerSegment = 16;
+    public AudioClip metronomeSound;
+    //public AudioClip[] clips = new AudioClip[2];
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.clip = metronomeSound;
+        audio.Play();
+        //public float startTime = Time.time;
+        //Debug.Log("The start time was: " + startTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<AudioSource>().Play();
+        UnityEngine.Debug.Log("Time at the start of this frame: " + Time.time);
+        UnityEngine.Debug.Log("deltaTime: " + Time.deltaTime);
+    }
+}
---
+++ UnityProjects/Project-SpellNote_Public/Assets/Code/test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public AudioClip otherClip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+
+        //audio.Play();
+        //yield return new WaitForSeconds(audio.clip.length);
+        audio.clip = otherClip;
+        audio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
